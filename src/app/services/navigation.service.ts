790c0a55dc4f6415dd3e2fefd2c98c5ad64aa47b
Erlend Eindride Fasmer <eefasmer@gmail.com>
2017-05-25 20:00:46 +0200

    added comment

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Subject, BehaviorSubject }    from 'rxjs';
 
 @Injectable()
+/**
+ * https://stackoverflow.com/questions/35884451/angular-2-sibling-component-communication
+ */
 export class NavigationService {
 
     currentPage: Subject<string> = new BehaviorSubject<string>('type');
